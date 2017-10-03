8fe6df34520bb18603d1ad7bd7034c66e46fa5d7
Masaki Samejima <samejima@ist.osaka-u.ac.jp>
2017-10-03 17:26:51 +0900

    complete RIF with scipy matrices

--- RIF.py
+++ RIF.py
@@ -5,7 +5,8 @@ Created on 2017/09/30
 
 import numpy as np
 import scipy.sparse.linalg
-from scipy.sparse import lil_matrix
+from scipy.sparse import lil_matrix, coo_matrix, csc_matrix, identity
+import time
 
 '''
 Left-looking Gram-Schmidt process for robust incomplete factorization
@@ -27,46 +28,43 @@ class RIF(object):
         
     def factorize(self):
         size = C.shape[0]
-        print("a")
-        Z = lil_matrix((size,size))
-        print("b")
+        Z = csc_matrix((size,size))
         L = lil_matrix((size,size))
         
-        print("c")
-        _Z =[lil_matrix((size,size)) for i in range(size)]
-        _Z[0][:,:] = np.identity(size)
-        
-        
-        print("d")
-        
-        for k in range(size):             
+        for k in range(size):    
+            _Z = identity(size, format = "csc")         
             if k > 0:
                 for j in range(k):
-                    L[j,k] = self.Cinner(_Z[j,:,k],Z[:,j])
-                    _Z[j+1,:,k] = _Z[j,:,k] - L[j,k]*Z[:,j]
-            
-            L[k,k] = self.Cnorm(_Z[k,:,k])
-            Z[:,k]= _Z[k,:,k] / L[k,k]
+                    L[j,k] = self.Cinner(_Z[:,k],Z[:,j])
+                    print(k,j,time.process_time())
+                    _Z[:,k] = _Z[:,k] - L[j,k]*Z[:,j]
+
+            L[k,k] = self.Cnorm(_Z[:,k])
+            Z[:,k]= _Z[:,k] / L[k,k]
        
         return Z, L
     
     def Cinner(self, x, y):
+        return x.T.dot(C.dot(y))[0,0]
+        #return x.dot(y.T)[0,0]
         #return np.matmul(np.matmul(x.T, self.C), y)
-        return x.T *self.C *y
+        #return (x.T *self.C *y)[0,0]
         
     def Cnorm(self, x):
         return np.sqrt(self.Cinner(x,x))
         
 if __name__ == '__main__':
     #A = np.asmatrix(np.random.rand(5,4))
-    matrix_size = np.array([20000,10000])
-    matrix_density = 0.01
-    A = scipy.sparse.rand(matrix_size[0],matrix_size[1], density = matrix_density ) 
+    
+    
+    matrix_size = np.array([200,100])
+    matrix_density = 0.1
+    
+    A = scipy.sparse.rand(matrix_size[0],matrix_size[1], density = matrix_density, format = 'csr') 
     C = A.T * A
-
     r = RIF(C);
     Z, L = r.factorize()
-    
+    print(time.process_time())
     '''
     Testing result
     - L^T is the Cholesky factor of C
@@ -76,7 +74,7 @@ if __name__ == '__main__':
     print("L^T")
     print(L.T)
     print("Cholesky factor")
-    print(np.linalg.cholesky(C))
+    print(np.linalg.cholesky(C.todense()))
     print("-----------------------------------------------")
     print("L^T *Z")
     print(L.T * Z)
